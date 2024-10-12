5da76c9cdca0711d2d3c2e5233e7c5311c4c9cd8
nionoku <nionoku@gmail.com>
2024-10-12 23:48:15 +0300

    feat: configure envs for packages

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite';
-import { join } from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import { join } from 'node:path';
 
 import rootPkg from '../../package.json';
 
-export default defineConfig({
-  base: join('/', rootPkg.name, 'core'),
-  build: {
-    outDir: join('..', '..', 'dist', 'core'),
-    emptyOutDir: true,
-  },
+export default defineConfig(({ mode }) => {
+  const rootPath = join('..', '..');
+  const viteEnv = loadEnv(mode, rootPath);  
+
+  const base = mode === 'production'
+    ? join('/', rootPkg.name, 'core')
+    : '/';
+
+  return {
+    base,
+    build: {
+      outDir: join(rootPath, 'dist', 'core'),
+      emptyOutDir: true,
+    },
+    server: {
+      port: Number(viteEnv.VITE_APP_CORE_PORT),
+    },
+  };
 });
