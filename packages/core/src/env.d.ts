9739071c3675e8ff556e0d50ec7955c44ce7f934
nionoku <nionoku@gmail.com>
2025-05-16 18:41:13 +0300

    update env types

--- packages/core/src/env.d.ts
+++ packages/core/src/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_CORE_BASE_URL: string
   readonly VITE_APP_CORE_TARGET_ORIGIN: string
+  readonly VITE_APP_APP_TARGET_ORIGIN: string
   /** @deprecated move value to config */
   readonly VITE_APP_RESOLVE_TIMEOUT: number
   // more env variables...
