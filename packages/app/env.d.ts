9bfdf0ee620952c17b15e0f3ab022e9d7cc4d231
nionoku <nionoku@gmail.com>
2025-05-17 16:30:23 +0300

    fix packages

--- packages/app/env.d.ts
+++ packages/app/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_CORE_BASE_URL: string
   readonly VITE_APP_CORE_TARGET_ORIGIN: string
+  readonly VITE_APP_APP_PORT: number
   // more env variables...
 }
 
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 import rootPkg from '../../package.json';
@@ -23,7 +22,6 @@ export default defineConfig(({ mode }) => {
     base,
     plugins: [
       vue(),
-      vueJsx(),
       vueDevTools(),
     ],
     resolve: {
@@ -36,7 +34,7 @@ export default defineConfig(({ mode }) => {
       outDir: path.join(rootPath, 'dist'),
     },
     server: {
-      port: Number(viteEnv.VITE_APP_APP_PORT),
+      port: Number(viteEnv['VITE_APP_APP_PORT']),
     },
   };
 });
