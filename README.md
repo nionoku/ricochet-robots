b3f7b65ec2e67232a68514c55df634a28f87f0e6
nionoku <nionoku@gmail.com>
2025-05-18 14:03:19 +0300

    update readme, remove debug code

--- README.md
+++ README.md
@@ -4,21 +4,42 @@
 
 ### Development
 
-First add listener for all messages
+1. add listener for all messages
 
 ```js
 window.addEventListener('message', (event) => console.log(event.data))
 ```
 
-Next prepare board
+2. Prepare board
 
 ```js
-window.postMessage({ event: 'prepare', schema: [ 0, 1, 2, 3 ], robotsCoords: { "blue": { "x": 14, "y": 5 }, "green": { "x": 9, "y": 12 }, "yellow": { "x": 10, "y": 2 }, "red": { "x": 15, "y": 9 }, "grey": { "x": 11, "y": 12 }} }, '*')
+window.postMessage({
+  event: 'core:configure_game',
+  order_map_parts: [ 0, 1, 2, 3 ],
+  robots_coords: {
+    "blue": [14, 5],
+    "green": [9, 12],
+    "yellow": [10, 2],
+    "red": [15, 9],
+    "grey": [11, 12]
+  }
+}, '*')
 
 ```
 
-And setup token
+3. Setup token
 
 ```js
-window.postMessage({ event: 'select_token', token: 'blue-gear' }, '*')
+window.postMessage({
+  event: 'core:set_target_token',
+  token: 'blue-gear'
+}, '*')
+```
+
+4. Enable moving robots
+
+```js
+window.postMessage({
+  event: 'core:enable_move_robots'
+}, '*')
 ```
--- packages/app/src/features/scene/component.vue
+++ packages/app/src/features/scene/component.vue
@@ -16,7 +16,7 @@
 
 <script lang="ts" setup>
 import { useTemplateRef, watch } from 'vue';
-import { CoreEvent, CoreEventsControllerImpl, type CoreMessageHandler } from '../../../../host';
+import { CoreEventsControllerImpl, type CoreMessageHandler } from '../../../../host';
 import { LoaderComponent } from './components/loader';
 import { useSceneState } from './composables/use-scene-state';
 
@@ -48,73 +48,16 @@ watch(isLoading, (value) => {
     .setContext(iframeContext);
 
   // DEBUG !!! REMOVE AFTER TESTS
-  /** @deprecated remove after tests */
-  const prepare = () => {
-    CoreEventsControllerImpl.instance
-      .sendMessage({
-        event: CoreEvent.PrepareGame,
-        order_map_parts: [0, 1, 2, 3],
-        robots_coords: {
-          blue: {
-            x: 14,
-            y: 5,
-          },
-          green: {
-            x: 9,
-            y: 12,
-          },
-          yellow: {
-            x: 10,
-            y: 2,
-          },
-          red: {
-            x: 15,
-            y: 9,
-          },
-          grey: {
-            x: 11,
-            y: 12,
-          },
-        },
-      });
-
-    CoreEventsControllerImpl.instance
-      .sendMessage({
-        event: CoreEvent.EnableMoveRobots,
-      });
-  };
-
-  // loading message handler
   const lmh: CoreMessageHandler = (event) => {
-    switch (event.data.event) {
-      case CoreEvent.Ready: {
-        // DEBUG !!! REMOVE AFTER TESTS
-        prepare();
-        // DEBUG !!! REMOVE AFTER TESTS
-
-        break;
-      }
-
-      case CoreEvent.MoveRobot:
-      case CoreEvent.RobotMoved:
-      case CoreEvent.SetTargetToken:
-      case CoreEvent.TokenAchieved:
-      case CoreEvent.SelectRobot: {
-        // TODO (2025.05.18): Move call sendMessage to peerjs-instance
-
-        // DEBUG !!! REMOVE AFTER TESTS
-        CoreEventsControllerImpl.instance
-          .sendMessage(event.data);
-        // DEBUG !!! REMOVE AFTER TESTS
-      }
-    }
+    // DEBUG !!! REMOVE AFTER TESTS
+    CoreEventsControllerImpl.instance
+      .sendMessage(event.data);
+    // DEBUG !!! REMOVE AFTER TESTS
   };
 
   // add message handler for watch ready state
   CoreEventsControllerImpl.instance
     .on(lmh);
-
-  prepare();
   // DEBUG !!! REMOVE AFTER TESTS
 });
 </script>
